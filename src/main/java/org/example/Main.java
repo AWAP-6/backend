f858b56e48449046460a3d21ecbbc86a319b04ee
R3ND3L <maxim.kantur@hotmail.com>
2023-11-01 18:30:45 +0200

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+
+import org.springframework.boot.SpringApplication;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        SpringApplication.run(Main.class, args);
+    }
+}
