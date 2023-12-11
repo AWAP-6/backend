d4a3928da898dbe87d57b42fdb0167c8f685efaa
Maksim Kantur <112544485+MaksimKantur@users.noreply.github.com>
2023-12-11 14:42:07 +0200

    logout functions

--- src/main/java/org/example/Config/SecurityConfig.java
+++ src/main/java/org/example/Config/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .cors()
                 .and()
                 .authorizeRequests()
-                .antMatchers("/", "/registration", "/users/test/**", "/login/**", "/lockers/**","/parcels/add" , "/users/delete/**","/lockers//{lockerId}/updateStatus" , "/lockers/checkOpenCode","/lockers/location/{locationId}/getStatus", "/lockers/lockerStatus","lockers/updateStatus", "/lockers/sendMail", "/activation/act")
+                .antMatchers("/", "/registration", "/logout" ,"/users/test/**", "/login/**", "/lockers/**","/parcels/add" , "/users/delete/**","/lockers//{lockerId}/updateStatus" , "/lockers/checkOpenCode","/lockers/location/{locationId}/getStatus", "/lockers/lockerStatus","lockers/updateStatus", "/lockers/sendMail", "/activation/act")
                 .permitAll()
                 .antMatchers(HttpMethod.POST, "/lockers/updateStatus").permitAll()
                 .anyRequest().authenticated()
@@ -35,6 +35,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin()
                 .disable()
                 .logout()
+                .logoutSuccessUrl("/hello")
                 .permitAll();
     }
 
