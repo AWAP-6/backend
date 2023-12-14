2aa9912f4713e30c358fcf2a7c741ba214d9f84a
R3ND3L <maxim.kantur@hotmail.com>
2023-12-14 10:43:53 +0200

    qf6

--- src/main/java/org/example/Config/CorsConfig.java
+++ src/main/java/org/example/Config/CorsConfig.java
@@ -6,18 +6,19 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.List;
+
 @Configuration
 public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOrigin("https://touchscreen.onrender.com");
+        configuration.setAllowedOrigins(List.of("https://touchscreen.onrender.com"));
         configuration.addAllowedMethod("*");
         configuration.addAllowedHeader("*");
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
-
         return source;
     }
 }
